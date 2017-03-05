b0f5371fa48cdae1eb055a13305e7497250a402a
JoaOliveir <joao.caseiro@tecnico.ulisboa.pt>
2017-03-05 15:18:28 +0000

    Activity: Task 2

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
@@ -5,8 +5,14 @@ import java.util.Set;
 
 import org.joda.time.LocalDate;
 
+import pt.ulisboa.tecnico.softeng.activity.domain.exception.ActivityException;
+
 public class Activity {
 	private static int counter = 0;
+
+	static final int MIN_AGE = 18;
+	static final int MAX_AGE = 99;
+	static final int MIN_CAPACITY = 1;
 
 	private final String name;
 	private final String code;
@@ -16,6 +22,10 @@ public class Activity {
 	private final Set<ActivityOffer> offers = new HashSet<>();
 
 	public Activity(ActivityProvider provider, String name, int minAge, int maxAge, int capacity) {
+
+		checkAge(minAge, maxAge);
+		checkCapacity(capacity);
+
 		this.code = provider.getCode() + Integer.toString(++Activity.counter);
 		this.name = name;
 		this.minAge = minAge;
@@ -23,6 +33,18 @@ public class Activity {
 		this.capacity = capacity;
 
 		provider.addActivity(this);
+	}
+
+	private void checkAge(int minAge, int maxAge){
+		if(minAge < MIN_AGE || maxAge > MAX_AGE || minAge > maxAge){
+			throw new ActivityException();
+		}
+	}
+
+	private void checkCapacity(int capacity){
+		if(capacity < MIN_CAPACITY){
+			throw new ActivityException();
+		}
 	}
 
 	String getName() {
