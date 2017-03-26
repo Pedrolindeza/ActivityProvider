92a821de681c9443f250bf5a750d726fae022697
Joao Trindade <joao_pedro_trindade@hotmail.com>
2017-03-26 19:30:40 +0100

    Grupo 1 - Refatorizar ProcessPaymentState

---
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ProcessPaymentState.java
@@ -0,0 +1,34 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+
+public class ProcessPaymentState extends AdventureState {
+	@Override
+	public State getState() {
+		return State.PROCESS_PAYMENT;
+	}
+
+	@Override
+	public void process(Adventure adventure) {
+		try {
+			adventure.setPaymentConfirmation ( BankInterface.processPayment(adventure.getIBAN(), adventure.getAmount()));
+		} catch (BankException be) {
+			adventure.setState(State.CANCELLED);
+		} catch (RemoteAccessException rae) {
+			// increment number of errors
+			// if (number of errors == 3) {
+			// setState(State.CANCELLED);
+			// }
+			return;
+		}
+
+		adventure.setState(State.RESERVE_ACTIVITY);
+
+		
+	}
+
+
+}
