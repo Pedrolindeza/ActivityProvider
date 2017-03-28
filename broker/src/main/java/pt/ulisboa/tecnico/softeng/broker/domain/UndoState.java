8fea22dfff0bbcafff48ff55da49da2aecd620ba
madalenassembleia <madalena.assembleia@tecnico.ulisboa.pt>
2017-03-29 00:37:06 +0100

    Grupo 5 - Refatorizar UndoState

---
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/UndoState.java
@@ -0,0 +1,48 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.hotel.exception.HotelException;
+
+public class UndoState extends AdventureState {
+	
+	@Override
+	public State getState(){
+		return State.UNDO;
+	}
+
+	@Override
+	public void process(Adventure adventure) {
+		if (adventure.cancelPayment()) {
+			try {
+				adventure.setPaymentCancellation(BankInterface.cancelPayment(adventure.getPaymentConfirmation()));
+			} catch (HotelException | RemoteAccessException ex) {
+				// does not change state
+			}
+		}
+	
+		if (adventure.cancelActivity()) {
+			try {
+				adventure.setActivityCancellation(ActivityInterface.cancelReservation(adventure.getActivityConfirmation()));
+			} catch (HotelException | RemoteAccessException ex) {
+				// does not change state
+			}
+		}
+	
+		if (adventure.cancelRoom()) {
+			try {
+				adventure.setRoomCancellation(HotelInterface.cancelBooking(adventure.getRoomConfirmation()));
+			} catch (HotelException | RemoteAccessException ex) {
+				// does not change state
+			}
+		}
+		 
+		if (!adventure.cancelPayment() && !adventure.cancelActivity() && !adventure.cancelRoom()) {
+			adventure.setState(State.CANCELLED);
+		}
+	
+	}
+}
