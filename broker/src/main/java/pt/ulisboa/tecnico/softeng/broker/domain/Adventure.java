2150ec3102df2f49c098495bd38ebfaf61027ed0
JoaOliveir <joao.caseiro@tecnico.ulisboa.pt>
2017-03-30 02:46:36 +0100

    Refatorização ConfirmedState

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/Adventure.java
@@ -164,7 +164,7 @@ public class Adventure {
 		case UNDO:
 			return this.state.getState();
 		case CONFIRMED:
-			return this.oldState;
+			return this.state.getState();
 		case CANCELLED:
 			return this.state.getState();
 		default:
@@ -189,7 +189,7 @@ public class Adventure {
 			this.state = new UndoState();
 			break;
 		case CONFIRMED:
-			this.state = null;
+			this.state = new ConfirmedState();
 			break;
 		case CANCELLED:
 			this.state = new CancelledState();
@@ -218,59 +218,7 @@ public class Adventure {
 			this.state.process(this);
 			break;
 		case CONFIRMED:
-			BankOperationData operation;
-			try {
-				operation = BankInterface.getOperationData(getPaymentConfirmation());
-			} catch (BankException be) {
-				// increment number of errors
-				// if (number of errors == 5) {
-				// adventure.setState(State.UNDO);
-				// }
-				// return;
-			} catch (RemoteAccessException rae) {
-				// increment number of errors
-				// if (number of errors == 20) {
-				// adventure.setState(State.UNDO);
-				// }
-				// return;
-			}
-			// reset number of errors
-
-			ActivityReservationData reservation;
-			try {
-				reservation = ActivityInterface.getActivityReservationData(getActivityConfirmation());
-			} catch (ActivityException ae) {
-				setState(State.UNDO);
-				return;
-			} catch (RemoteAccessException rae) {
-				// increment number of errors
-				// if (number of errors == 20) {
-				// adventure.setState(State.UNDO);
-				// }
-				// return;
-			}
-			// reset number of errors
-
-			if (getRoomConfirmation() != null) {
-				RoomBookingData booking;
-				try {
-					booking = HotelInterface.getRoomBookingData(getRoomConfirmation());
-				} catch (HotelException he) {
-					setState(State.UNDO);
-					return;
-				} catch (RemoteAccessException rae) {
-					// increment number of errors
-					// if (number of errors == 20) {
-					// adventure.setState(State.UNDO);
-					// }
-					// return;
-				}
-				// reset number of errors
-			}
-
-			// TODO: prints the complete Adventure file, the info in operation,
-			// reservation and booking
-
+			this.state.process(this);
 			break;
 		case CANCELLED:
 			this.state.process(this);
