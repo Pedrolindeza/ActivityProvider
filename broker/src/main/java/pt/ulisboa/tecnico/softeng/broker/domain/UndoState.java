395b2c69f1e24d20a3097ee57a9a7eafcfe22529
madalenassembleia <madalena.assembleia@tecnico.ulisboa.pt>
2017-03-30 23:09:57 +0100

    Implementar UndoState fix

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/UndoState.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/UndoState.java
@@ -42,7 +42,7 @@ public class UndoState extends AdventureState {
 		}
 		 
 		if (!adventure.cancelPayment() && !adventure.cancelActivity() && !adventure.cancelRoom()) {
-			adventure.setState(State.CONFIRMED);
+			adventure.setState(State.CANCELLED);
 		}
 	
 	}
