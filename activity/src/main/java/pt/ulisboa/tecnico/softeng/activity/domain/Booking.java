26c0e8cc044d6c300043fa347f83f2268bbd3cd6
Joao Trindade <joao_pedro_trindade@hotmail.com>
2017-04-23 15:19:31 +0100

    Persistencia dos atributos e persistencia entre classes - Booking

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Booking.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Booking.java
@@ -7,14 +7,11 @@ import pt.ulisboa.tecnico.softeng.activity.exception.ActivityException;
 public class Booking extends Booking_Base{
 	private static int counter = 0;
 
-	private final String reference;
-	private String cancel;
-	private LocalDate cancellationDate;
 
 	public Booking(ActivityProvider provider, ActivityOffer offer) {
 		checkArguments(provider, offer);
 
-		this.reference = provider.getCode() + Integer.toString(++Booking.counter);
+		setReference(provider.getCode() + Integer.toString(++Booking.counter));
 
 		offer.addBookingToSet(this);
 	}
@@ -25,25 +22,14 @@ public class Booking extends Booking_Base{
 		}
 	}
 
-	public String getReference() {
-		return this.reference;
-	}
-
-	public String getCancellation() {
-		return this.cancel;
-	}
-
-	public LocalDate getCancellationDate() {
-		return this.cancellationDate;
-	}
 
 	public String cancel() {
-		this.cancel = "CANCEL" + this.reference;
-		this.cancellationDate = new LocalDate();
-		return this.cancel;
+		setCancel("CANCEL" + getReference());
+		setCancellationDate(new LocalDate());
+		return getCancel();
 	}
 
 	public boolean isCancelled() {
-		return this.cancel != null;
+		return getCancel() != null;
 	}
 }
