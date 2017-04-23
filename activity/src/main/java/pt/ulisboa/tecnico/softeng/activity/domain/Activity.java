b6cc5edbe28f3050ebbc54095c03328b30f58873
jcalisto <joao.s.calisto@tecnico.ulisboa.pt>
2017-04-23 13:26:57 +0100

    #77

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/domain/Activity.java
@@ -19,7 +19,6 @@ public class Activity extends Activity_Base{
 	private final int minAge;
 	private final int maxAge;
 	private final int capacity;
-	private final Set<ActivityOffer> offers = new HashSet<>();
 
 	public Activity(ActivityProvider provider, String name, int minAge, int maxAge, int capacity) {
 		checkArguments(provider, name, minAge, maxAge, capacity);
@@ -69,16 +68,16 @@ public class Activity extends Activity_Base{
 	}
 
 	int getNumberOfOffers() {
-		return this.offers.size();
+		return getActivityOfferSet().size();
 	}
 
 	void addOffer(ActivityOffer offer) {
-		this.offers.add(offer);
+		addActivityOffer(offer);
 	}
 
 	Set<ActivityOffer> getOffers(LocalDate begin, LocalDate end, int age) {
 		Set<ActivityOffer> result = new HashSet<>();
-		for (ActivityOffer offer : this.offers) {
+		for (ActivityOffer offer : getActivityOfferSet()) {
 			if (matchAge(age) && offer.available(begin, end)) {
 				result.add(offer);
 			}
@@ -91,7 +90,7 @@ public class Activity extends Activity_Base{
 	}
 
 	public Booking getBooking(String reference) {
-		for (ActivityOffer offer : this.offers) {
+		for (ActivityOffer offer : getActivityOfferSet()) {
 			Booking booking = offer.getBooking(reference);
 			if (booking != null) {
 				return booking;
@@ -101,7 +100,7 @@ public class Activity extends Activity_Base{
 	}
 
 	public Set<ActivityOffer> getOffers() {
-		return this.offers;
+		return getActivityOfferSet();
 	}
 
 }
